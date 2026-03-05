66a6d3a5970fb562163d5b41818f22409d25f090
zaaraahmad1 <za10196@st.habib.edu.pk>
2026-03-05 11:31:21 +0500

    lab work

---
+++ Lab 7/Lab 7.srcs/sim_1/new/RF_ALU_FSM_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/05/2026 09:41:23 AM
+// Design Name: 
+// Module Name: RF_ALU_FSM_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RF_ALU_FSM_tb(
+
+    );
+endmodule
---
+++ Lab 7/Lab 7.srcs/sources_1/new/RegisterFile.v
@@ -0,0 +1,55 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/05/2026 09:33:08 AM
+// Design Name: 
+// Module Name: RegisterFile
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module RegisterFile(
+    input clk,
+    input rst,
+    input WriteEnable,
+    input  [4:0] rs1,
+    input  [4:0] rs2,
+    input  [4:0] rd,
+    input  [31:0] WriteData,
+    output [31:0] ReadData1,
+    output [31:0] ReadData2
+);
+
+    reg [31:0] regs [31:0];
+
+    integer i;
+
+    // reset registers
+    always @(posedge clk or posedge rst) begin
+        if(rst) begin
+            for(i=0;i<32;i=i+1)
+                regs[i] <= 32'b0;
+        end
+        else begin
+            if(WriteEnable && rd != 5'd0)
+                regs[rd] <= WriteData;
+        end
+    end
+
+    assign ReadData1 = (rs1 == 5'd0) ? 32'b0 : regs[rs1];
+
+    assign ReadData2 = (rs2 == 5'd0) ? 32'b0 : regs[rs2];
+
+endmodule
+ 
