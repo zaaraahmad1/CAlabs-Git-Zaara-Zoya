134a78f166ca600d77f094cdb8d128c2ad5bf63d
zaaraahmad1 <za10196@st.habib.edu.pk>
2026-03-20 00:03:38 +0500

    lab 8 final

---
+++ Lab 8/project_lab8latest.srcs/sources_1/new/ALUControl.v
@@ -0,0 +1,52 @@
+`timescale 1ns / 1ps
+
+module ALUControl(
+
+input [1:0] ALUOp,
+input [2:0] funct3,
+input [6:0] funct7,
+
+output reg [3:0] ALUControl
+
+);
+
+always @(*) begin
+
+case(ALUOp)
+
+// load/store ? ADD
+2'b00: ALUControl = 4'b0000;
+
+// branch ? SUB
+2'b01: ALUControl = 4'b0001;
+
+// R-type / I-type
+2'b10: begin
+
+    case(funct3)
+
+    3'b000: begin
+        if(funct7 == 7'b0100000)
+            ALUControl = 4'b0001; // SUB
+        else
+            ALUControl = 4'b0000; // ADD
+    end
+
+    3'b111: ALUControl = 4'b0010; // AND
+    3'b110: ALUControl = 4'b0011; // OR
+    3'b100: ALUControl = 4'b0100; // XOR
+    3'b001: ALUControl = 4'b0101; // SLL
+    3'b101: ALUControl = 4'b0110; // SRL
+
+    default: ALUControl = 4'b0000;
+
+    endcase
+end
+
+default: ALUControl = 4'b0000;
+
+endcase
+
+end
+
+endmodule
